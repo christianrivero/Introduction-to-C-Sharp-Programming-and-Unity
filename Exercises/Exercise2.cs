1caffaadf648b887215f7df0770a3d3774d40408
Christian Rivero <42509107+christianrivero@users.noreply.github.com>
2021-05-30 11:29:36 -0500

    Adds Exercise2 file

---
+++ Exercises/Exercise2.cs
@@ -0,0 +1,14 @@
+/*
+Problem 2 – Output your best friend's or your nemesis' name
+Add code to output the name of your best friend or your nemesis.
+*/
+using System;
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("My name is Christian Rivero.");
+            Console.WriteLine("My best friend's name is Sergio Cabrera.");
+        }
+    }
+}
